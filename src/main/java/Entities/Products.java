79c4469b6da498f5e46106c824e2640db66a62b0
Kaic <kaicsalha@icloud.com>
2026-03-13 16:27:54 -0300

    Add product management methods to Products class

--- src/main/java/Entities/Products.java
+++ src/main/java/Entities/Products.java
@@ -1,4 +1,32 @@
 package Entities;
 
 public class Products {
+
+    public String name;
+    public double price;
+    public int quantity;
+
+    public double totalValueInStock() {
+        return price * quantity;
+    }
+
+    public void addProducts(int quantity){
+        this.quantity += quantity;
+
+    }
+
+    public void removeProducts(int quantity){
+        this.quantity -= quantity;
+    }
+
+    public String toString(){
+
+        return name
+                + ", R$"
+                + String.format("%.2f", price)
+                + ", "
+                + quantity
+                + " units, Total : R$ "
+                + String.format("%.2f",totalValueInStock());
+    }
 }
